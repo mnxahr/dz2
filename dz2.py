cc33635bb41aa0d0477d8a910fd3557d4c353d5c
mnxahr <mnxahr235@gmail.com>
2023-08-22 16:28:47 +0300

    cjj,gj

--- dz2.py
+++ dz2.py
@@ -1,5 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles1.css">
+  <title>Document</title>
+</head>
 
-def is_palindrome(s):
-    return s == s[::-1]
-s1 = input()
-print(is_palindrome(s1))
+<body>
+  
+</body>
+  <h1>Заголовок 1 уровня</h1>
+  <p class="paragraph-1">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+    Expedita consequuntur ab praesentium blanditiis. 
+    Expedita repellat vel eveniet, necessitatibus modi error enim 
+    illum, deleniti repudiandae cupiditate dicta animi aut? Veritatis, voluptas?
+  </p>
+  <h2>Заголовок 2 уровня</h2>
+  <p class="paragraph-3">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+    Expedita consequuntur ab praesentium blanditiis. 
+    Expedita repellat vel eveniet, necessitatibus modi error enim 
+    illum, deleniti repudiandae cupiditate dicta animi aut? Veritatis, voluptas?
+  </p>
+  <h3>Заголовок 3 уровня</h3>
+  <h4>Заголовок 4 уровня</h4>
+  <h5>Заголовок 5 уровня</h5>
+  <h6>Заголовок 6 уровня</h6>
+  <h3>Планы на сегодня</h3>
+  <ul>
+    <li>Искупаться в озере</li>
+    <li>Поспать</li>
+    <li>Генеральная уборка</li>
+    <li>Поиграть в доту</li>
+  </ul>
+  <img src="skelet.jpg" />
+</html>
+
+css:
+h1 {
+    background: red;
+}
+
+.paragraph-1 {
+    background: blueviolet;
+    font-size: larger;
+    color: aqua;
+}
+.paragraph-2 {
+    font-size: 46pt;
+    border: 10px solid black;
+    border-radius: 5px;
+    color:brown;
+} 
+.paragraph-3 {
+    font-style: italic;
+     text-align: center;
+    font-size: 24px;
+    color: green;
+    text-shadow: 2px 2px green;
