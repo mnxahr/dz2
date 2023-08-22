a5a962b70a9b1b882cb7a02a1df1e0cd72f1a7dd
mnxahr <mnxahr235@gmail.com>
2023-08-22 20:11:07 +0300

    lesson3

--- dz2.py
+++ dz2.py
@@ -50,3 +50,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"></script>
 </body>
 </html>
+.
